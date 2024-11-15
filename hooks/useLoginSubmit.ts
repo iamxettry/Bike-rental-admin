d247a6c0b87beebb9f759a8c2e48696c2c51a731
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-15 19:58:30 +0545

    user login hook add

---
+++ hooks/useLoginSubmit.ts
@@ -0,0 +1,55 @@
+import { LoginSchemaType } from "@/Auth/Schema/LoginSchema";
+import { successResponse } from "@/Auth/types/common";
+// import UserServices from "@/services/UserServices";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useFormContext } from "react-hook-form";
+import toast from "react-hot-toast";
+
+const useLoginSubmit = () => {
+  const router = useRouter();
+  const { register, watch, handleSubmit, setError, formState } =
+    useFormContext<LoginSchemaType>();
+
+  const onSubmit: SubmitHandler<LoginSchemaType> = async (data) => {
+    const newPromise: Promise<successResponse> = new Promise(
+      async (resolve, reject) => {
+        try {
+          // const response = await UserServices.loginUser(data);
+          console.log(data);
+          // resolve(response);
+        } catch (error) {
+          if (error instanceof AxiosError && error.response?.data?.detail) {
+            const errMsg = error?.response?.data?.detail;
+            setError("email", {});
+            setError("password", {});
+            reject(errMsg);
+          } else if (error instanceof Error) {
+            reject(error?.message);
+          } else {
+            reject("Network Error!!");
+          }
+        }
+      }
+    );
+    await toast.promise(newPromise, {
+      loading: "Logging in...",
+      success: (response) => {
+        let msg;
+        if (response?.otp_created_at) {
+          sessionStorage.setItem("email", data.email);
+          msg = response?.success || "OTP sent to your email!";
+          router.push("/auth/login?verifyOtp=true");
+        } else {
+          msg = response?.success || "Login successful!";
+          router.push("/");
+        }
+        return msg;
+      },
+      error: (err) => err,
+    });
+  };
+  return { register, handleSubmit, watch, formState, onSubmit };
+};
+
+export default useLoginSubmit;
