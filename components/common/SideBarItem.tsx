d566dc7319ead7a8d71e20704fbe5ed584b7983d
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-16 08:57:27 +0545

    fix : sidebar

--- components/common/SideBarItem.tsx
+++ components/common/SideBarItem.tsx
@@ -15,7 +15,7 @@ const SideBarItem = ({ item }: { item: SidebarType }) => {
   return (
     <>
       <div
-        className={`p-2 rounded-md hover:bg-blue-100  ${
+        className={`py-2  px-4 rounded-md hover:bg-blue-100  ${
           pathname === item?.path && "bg-blue-200"
         } `}
       >
--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -2,11 +2,12 @@ import { WithMyBike } from "@/assets";
 import Image from "next/image";
 import React from "react";
 import SidebarList from "./SidebarList";
+import LogOut from "./LogOut";
 
 const Sidebar = () => {
   return (
     <div className="col-span-10 sm:col-span-4 md:col-span-3 lg:col-span-2  space-y-2 ">
-      <div className="">
+      <div className="bg-blue-200">
         <Image
           src={WithMyBike}
           width={400}
@@ -15,8 +16,9 @@ const Sidebar = () => {
           className="w-full object-cover"
         />
       </div>
-      <div className="p-6 ">
+      <div className="p-4 ">
         <SidebarList />
+        <LogOut />
       </div>
     </div>
   );
