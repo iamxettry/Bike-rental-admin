0c60f9bec40e1cac49f9a31102815d9e155fbb35
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-17 22:21:23 +0545

    feat :add bike service integration

--- hooks/useBikeSubmit.ts
+++ hooks/useBikeSubmit.ts
@@ -1,13 +1,48 @@
+import { successResponse } from "@/Auth/types/common";
+import BikeServices from "@/Bikes/services/BikeServices";
 import { bikeType } from "@/Bikes/types/bikeSchema";
+import { AxiosError } from "axios";
 import { SubmitHandler, useFormContext } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const useBikeSubmit = () => {
-  const { handleSubmit, formState } = useFormContext<bikeType>();
+  const { handleSubmit, watch, setValue, formState } =
+    useFormContext<bikeType>();
+  const watchImage = watch("image");
   const onSubmit: SubmitHandler<bikeType> = async (data) => {
-    console.log("data", data);
+    // if not image is selected, set image to null
+    if (!watchImage) {
+      delete data.image;
+    }
+
+    const newPromise: Promise<successResponse> = new Promise(
+      async (resolve, reject) => {
+        try {
+          const response = await BikeServices.postBike(data);
+          resolve(response);
+        } catch (error) {
+          console.log("error", error);
+          if (error instanceof AxiosError && error.response?.data) {
+            const errMsg =
+              error?.response?.data?.detail || error?.response?.data?.data;
+            reject(errMsg);
+          } else if (error instanceof Error) {
+            reject(error?.message);
+          } else {
+            reject("Network Error!!");
+          }
+        }
+      }
+    );
+    toast.promise(newPromise, {
+      loading: "Loading",
+      success: (res) => res.success || "Bike added successfully",
+      error: (err) => err || "Failed to add bike",
+    });
   };
   return {
     handleSubmit: handleSubmit(onSubmit),
+    watch,
     formState,
   };
 };
