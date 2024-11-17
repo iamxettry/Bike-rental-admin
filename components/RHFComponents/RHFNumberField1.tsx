90fea595fa9f4d1a686be3d59028c4f4befad8e8
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-17 09:12:07 +0545

    add number field

---
+++ components/RHFComponents/RHFNumberField1.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
+import { TextField, TextFieldProps } from "@mui/material";
+import { orange } from "@mui/material/colors";
+
+type Props<T extends FieldValues> = {
+  name: Path<T>;
+} & Pick<TextFieldProps, "label" | "variant" | "placeholder">;
+
+const RHFNumberField = <T extends FieldValues>({
+  name,
+  ...props
+}: Props<T>) => {
+  const { control } = useFormContext<T>();
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.replace(/\D/g, "");
+    event.target.value = value;
+  };
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          {...props}
+          type="text"
+          error={!!error}
+          helperText={error?.message}
+          size="small"
+          autoComplete="off"
+          onInput={handleInputChange}
+          onChange={(e) => {
+            const value = e.target.value ? Number(e.target.value) : "";
+            field.onChange(value);
+          }}
+          sx={{
+            "& label.Mui-focused": {
+              color: orange[700],
+            },
+            "& label": {
+              color: error ? "red" : undefined,
+            },
+            "& .MuiOutlinedInput-root": {
+              "&:hover fieldset": {
+                borderColor: orange[700],
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: orange[700],
+              },
+            },
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default RHFNumberField;
