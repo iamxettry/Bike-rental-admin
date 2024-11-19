f044ef90d22f3affe64cb7e3ae13d4e69a6da9ef
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-19 21:46:39 +0545

    add bike list

--- Bikes/components/BikeList.tsx
+++ Bikes/components/BikeList.tsx
@@ -1,65 +1,40 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect } from "react";
 import BikeServices from "../services/BikeServices";
+import BikeListTable from "./BikeListTable";
 import Loading from "@/components/utils/Loading";
-import { BikeComponent } from "@/components/global/Bike";
-import { Bike, BikeListResponse } from "../types/bikeApiTypes";
-import { useBikeStore } from "@/store/bikeStore";
-import Notfound from "@/components/global/Notfound";
+import Notfound from "@/components/common/Notfound";
+import useBikeSubmit from "@/hooks/useBikeSubmit";
 
 const BikeList = () => {
-  const { searchQuery, bikes, setBikes, isLoading, setIsLoading } =
-    useBikeStore((state) => ({
-      searchQuery: state.searchQuery,
-      bikes: state.bikes,
-      setBikes: state.setBikes,
-      isLoading: state.isLoading,
-      setIsLoading: state.setIsLoading,
-    }));
+  const { handleFeaturedStatus } = useBikeSubmit();
 
-  const { data, isLoading: bikeLoading } = useQuery<BikeListResponse>({
-    queryFn: async () => await BikeServices.getBikeList(),
-    queryKey: ["get-bike-list"],
-    select: (res) => res,
+  const {
+    data: BikeList,
+    isFetching,
+    refetch,
+  } = useQuery({
+    queryKey: ["BikeList"],
+    queryFn: async () => BikeServices.getBikeList(),
   });
 
-  useEffect(() => {
-    if (searchQuery) {
-      setIsLoading(true);
-      const timer = setTimeout(async () => {
-        const response = await BikeServices.searchBikes(searchQuery);
-        setIsLoading(false);
-        setBikes(response);
-      }, 500);
-      return () => clearTimeout(timer);
-    } else {
-      if (data) {
-        setBikes(data);
-        setIsLoading(bikeLoading);
-      }
-    }
-  }, [searchQuery, data, bikeLoading]);
   return (
     <div className="">
-      {isLoading ? (
-        <div className="h-32 flex justify-center items-center  mt-12 ">
-          <Loading className="text-7xl text-gray-600 !important" />
+      {isFetching ? (
+        <div className="h-96 flex justify-center items-center">
+          <Loading />
         </div>
+      ) : BikeList && BikeList?.length > 0 ? (
+        <BikeListTable
+          data={BikeList}
+          handleFeaturedStatus={(id, bike) => {
+            handleFeaturedStatus(id, bike);
+            // Optionally manually refetch the bike list here
+            // refetch();
+          }}
+        />
       ) : (
-        <div className="grid place-items-center md:grid-cols-2 xl:grid-cols-3 py-12 gap-4">
-          {bikes?.length > 0 ? (
-            bikes?.map((bike: Bike) => (
-              <div key={bike.id}>
-                <BikeComponent bike={bike} />
-              </div>
-            ))
-          ) : (
-            <div className="col-span-3 w-full flex justify-center ">
-              <Notfound msg="Sorry, no bikes found matching your search. Please try different keywords or adjust your filters." />
-            </div>
-          )}
-        </div>
+        <Notfound msg="No Data" />
       )}
     </div>
   );
