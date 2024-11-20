4a1ae52237e9a6288d612d92d291be86f1a0ed29
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-20 09:53:15 +0545

    update store

--- store/modalStore.ts
+++ store/modalStore.ts
@@ -1,17 +1,27 @@
 import { create } from "zustand";
 type State = {
   isOpen: boolean;
+  editId: string;
+  preview: string | null;
 };
 
 type Action = {
   openModal: () => void;
   closeModal: () => void;
+  setEditId: (value: string) => void;
+  setPreview: (value: string | null) => void;
 };
 
 const modalStore = create<State & Action>((set) => ({
   isOpen: false,
   openModal: () => set({ isOpen: true }),
-  closeModal: () => set({ isOpen: false }),
+  closeModal: () => set({ isOpen: false, editId: "", preview: null }),
+
+  editId: "",
+  setEditId: (value) => set({ editId: value }),
+
+  preview: null,
+  setPreview: (value) => set({ preview: value }),
 }));
 
 export default modalStore;
