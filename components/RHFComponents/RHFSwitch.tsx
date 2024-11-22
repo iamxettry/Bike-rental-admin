563e842ebb49d4b9ce988b93477cec9d82d55717
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-22 13:13:33 +0545

    feat : RHF switch button added

--- components/RHFComponents/RHFSwitch.tsx
+++ components/RHFComponents/RHFSwitch.tsx
@@ -1,5 +1,6 @@
 import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
-import { FormControlLabel, Switch } from "@mui/material";
+import { Switch } from "@headlessui/react";
+
 type Props<T extends FieldValues> = {
   name: Path<T>;
   label: string;
@@ -13,10 +14,22 @@ const RHFSwitch = <T extends FieldValues>({ name, label }: Props<T>) => {
       control={control}
       name={name}
       render={({ field }) => (
-        <FormControlLabel
-          control={<Switch {...field} checked={field.value} />}
-          label={label}
-        />
+        <div className="flex items-center space-x-2 gap-5">
+          <label className="text-sm font-medium text-gray-900">{label}</label>
+          <Switch
+            checked={field.value}
+            onChange={field.onChange}
+            className={`group inline-flex  h-5 w-9 items-center rounded-full transition ${
+              field.value ? "bg-primary" : "bg-gray-400"
+            }`}
+          >
+            <span
+              className={`inline-block h-3 w-3 transform rounded-full bg-white transition ${
+                field.value ? "translate-x-5" : "translate-x-1"
+              }`}
+            />
+          </Switch>
+        </div>
       )}
     />
   );
