9978ce80958d5cb2a848538486f576956bc50b4a
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-24 19:42:11 +0545

    add location fetch services

--- services/LocationServices.ts
+++ services/LocationServices.ts
@@ -11,6 +11,38 @@ const LocationService = {
       Authorization: `Bearer ${await getCookies()}`,
     });
   },
+  // Fetch location data by id
+  getLocationById: async (id: string) => {
+    return requests.get(`common/location-retrieve-update-delete/${id}/`, {
+      Authorization: `Bearer ${await getCookies()}`,
+    });
+  },
+
+  // Fetch all locations
+  getLocationList: async () => {
+    return requests.get("common/location-list/", {
+      Authorization: `Bearer ${await getCookies()}`,
+    });
+  },
+  // Update location data
+  updateLocation: async (id: string, data: LocationType) => {
+    return requests.put(`common/location-retrieve-update-delete/${id}/`, data, {
+      Authorization: `Bearer ${await getCookies()}`,
+    });
+  },
+  // Delete location data
+  deleteLocation: async (id: string) => {
+    return requests.delete(`common/location-retrieve-update-delete/${id}/`, {
+      Authorization: `Bearer ${await getCookies()}`,
+    });
+  },
+
+  // Searach location data
+  searchLocation: async (query: string) => {
+    return requests.get(`common/location-search/?search=${query}`, {
+      Authorization: `Bearer ${await getCookies()}`,
+    });
+  },
 };
 
 export default LocationService;
