d5344fcdb302c86043265fd1099d066d8e4d2800
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-24 19:41:16 +0545

    Location response type add

--- settings/types/locationSchema.ts
+++ settings/types/locationSchema.ts
@@ -1,11 +1,18 @@
 import { z } from "zod";
 
 export const locationSchema = z.object({
-  city: z.string().min(1),
+  city: z.string().min(1, "City is required"),
 });
 
 export type LocationType = z.infer<typeof locationSchema>;
 
 export const defaultLocation: LocationType = {
   city: "",
+};
+
+export type LocationListResponse = {
+  id: string;
+  city: string;
+  created_at: string;
+  updated_at: string;
 };
