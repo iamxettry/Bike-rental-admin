ef6f4c1a216b7798e7901cf06b1682758896df18
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-24 20:14:49 +0545

    add type for form

--- settings/components/LocationProvider.tsx
+++ settings/components/LocationProvider.tsx
@@ -2,10 +2,14 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormProvider, useForm } from "react-hook-form";
-import { defaultLocation, locationSchema } from "../types/locationSchema";
+import {
+  defaultLocation,
+  locationSchema,
+  LocationType,
+} from "../types/locationSchema";
 
 const LocationProvider = ({ children }: { children: React.ReactNode }) => {
-  const methods = useForm({
+  const methods = useForm<LocationType>({
     mode: "all",
     resolver: zodResolver(locationSchema),
     defaultValues: defaultLocation,
