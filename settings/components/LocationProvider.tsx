f99ca3bc3ccc41c3db2157ce902147c42336fd03
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-24 19:32:20 +0545

    add form default value and resolver

--- settings/components/LocationProvider.tsx
+++ settings/components/LocationProvider.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { FormProvider, useForm } from "react-hook-form";
+import { defaultLocation, locationSchema } from "../types/locationSchema";
 
 const LocationProvider = ({ children }: { children: React.ReactNode }) => {
-  const methods = useForm();
+  const methods = useForm({
+    mode: "all",
+    resolver: zodResolver(locationSchema),
+    defaultValues: defaultLocation,
+  });
   return <FormProvider {...methods}>{children}</FormProvider>;
 };
 
