da9f2684c47e41f4166d29458e3b38c29fcf89b1
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-24 09:37:40 +0545

    use global serch

--- Bikes/components/AddBikeNav.tsx
+++ Bikes/components/AddBikeNav.tsx
@@ -4,7 +4,7 @@ import { Button } from "@mui/material";
 import React from "react";
 import { useFormContext } from "react-hook-form";
 import { defaultBikeValues } from "../types/bikeSchema";
-import SearchBikes from "./SearchBikes";
+import Search from "@/components/common/Search";
 
 const AddBikeNav = () => {
   const { openDrawer } = useModal();
@@ -16,7 +16,7 @@ const AddBikeNav = () => {
   };
   return (
     <div className=" flex flex-col md:flex-row gap-4 justify-between items-center">
-      <SearchBikes />
+      <Search placeholder="Search bike by name, model, brand" />
       <Button
         variant="outlined"
         className="  !text-sm md:!text-base  !capitalize   hover:!text-primary !text-black   !border-primary  "
