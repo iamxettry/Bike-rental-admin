8518864595f89dab5d0802b78156430bde66c39f
Raju Chhetri <rajuchhetry11@gmail.com>
2025-01-09 13:02:44 +0545

    feat: dashbord graph

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,3 +1,37 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Home() {
-  return <div>Dashboard </div>;
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Quick Stats</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="p-4 bg-blue-100 rounded-lg">
+              <p className="text-sm text-blue-600 font-medium">Total Bikes</p>
+              <p className="text-2xl font-bold">100</p>
+            </div>
+            <div className="p-4 bg-green-100 rounded-lg">
+              <p className="text-sm text-green-600 font-medium">
+                Active Rentals
+              </p>
+              <p className="text-2xl font-bold">45</p>
+            </div>
+            <div className="p-4 bg-yellow-100 rounded-lg">
+              <p className="text-sm text-yellow-600 font-medium">
+                Today's Revenue
+              </p>
+              <p className="text-2xl font-bold">$890</p>
+            </div>
+            <div className="p-4 bg-purple-200 rounded-lg">
+              <p className="text-sm text-purple-600 font-medium">New Users</p>
+              <p className="text-2xl font-bold">24</p>
+            </div>
+          </div>
+        </CardContent>
+      </Card>{" "}
+    </div>
+  );
 }
